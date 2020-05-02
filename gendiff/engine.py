266298191abe738e2b2120286aac27882599aae2
Artlyne <Artlynee@gmail.com>
2020-05-02 23:22:28 +0500

    feat: flat file comparison (JSON)

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,2 +1,26 @@
+import json
+SAME, ADDED, REMOVED = ' ', '+', '-'
+
+
 def generate_diff(path_to_file1, path_to_file2):
-    pass
+    file1 = json.load(open(path_to_file1))
+    file2 = json.load(open(path_to_file2))
+
+    result = ''
+    for key, value in file1.items():
+        if key not in file2.keys():
+            result += f'{REMOVED} {key}: {value}\n'
+            continue
+
+        if value == file2[key]:
+            result += f'{SAME} {key}: {value}\n'
+        elif value != file2[key]:
+            result += f'{ADDED} {key}: {file2[key]}\n'
+            result += f'{REMOVED} {key}: {value}\n'
+
+    for key, value in file2.items():
+        if key not in file1.keys():
+            result += f'{ADDED} {key}: {value}\n'
+
+    print(result)
+    return result
