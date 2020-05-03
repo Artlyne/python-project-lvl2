6fea3fe7dce315417c9590f67b48065a0d4caf5e
Artlyne <Artlynee@gmail.com>
2020-05-03 14:00:30 +0500

    refactor: improve readability and reduce complexity

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,26 +1,39 @@
+"""Finds the difference between two JSON files."""
 import json
+
 SAME, ADDED, REMOVED = ' ', '+', '-'
 
 
 def generate_diff(path_to_file1, path_to_file2):
-    file1 = json.load(open(path_to_file1))
-    file2 = json.load(open(path_to_file2))
+    """Find the difference between two JSON files.
 
-    result = ''
-    for key, value in file1.items():
-        if key not in file2.keys():
-            result += f'{REMOVED} {key}: {value}\n'
-            continue
+    Args:
+        path_to_file1 (str): specify the path to the file1
+        path_to_file2 (str): specify the path to the file2
 
-        if value == file2[key]:
-            result += f'{SAME} {key}: {value}\n'
-        elif value != file2[key]:
-            result += f'{ADDED} {key}: {file2[key]}\n'
-            result += f'{REMOVED} {key}: {value}\n'
+    Returns:
+        str
+    """
+    before = json.load(open(path_to_file1))
+    after = json.load(open(path_to_file2))
 
-    for key, value in file2.items():
-        if key not in file1.keys():
-            result += f'{ADDED} {key}: {value}\n'
+    same_keys = before.keys() & after.keys()
+    removed_keys = before.keys() - after.keys()
+    added_keys = after.keys() - before.keys()
 
-    print(result)
-    return result
+    difference = ''
+
+    for key in same_keys:
+        if before[key] == after[key]:
+            difference += f'{SAME} {key}: {before[key]}\n'
+        else:
+            difference += f'{ADDED} {key}: {after[key]}\n'
+            difference += f'{REMOVED} {key}: {before[key]}\n'
+
+    for key in removed_keys:
+        difference += f'{REMOVED} {key}: {before[key]}\n'
+
+    for key in added_keys:
+        difference += f'{ADDED} {key}: {after[key]}\n'
+
+    return difference
