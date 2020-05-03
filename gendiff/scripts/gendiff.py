67b092ba42737bc97682be6f49831101eecbc9c3
Artlyne <Artlynee@gmail.com>
2020-05-03 14:06:54 +0500

    refactor: print result

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+"""gendiff script."""
 import argparse
 from gendiff.engine import generate_diff
 
 
 def main():
+    """Run this script."""
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    generate_diff(args.first_file, args.second_file)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
