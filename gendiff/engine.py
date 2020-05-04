24a8163494e981d831fc9468cafefcf3b7aed219
Artlyne <Artlynee@gmail.com>
2020-05-04 21:07:47 +0500

    refactor: add curly braces at the beginning and end of the output string

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,7 +1,7 @@
 """Finds the difference between two JSON files."""
 import json
 
-SAME, REMOVED, ADDED = ' ', '-', '+'
+SAME, REMOVED, ADDED = '   ', '  -', '  +'
 
 
 def generate_diff(path_to_file1, path_to_file2):
@@ -12,10 +12,10 @@ def generate_diff(path_to_file1, path_to_file2):
         path_to_file2 (str): specify the path to the file2
 
     Returns:
-        str: difference between files, where first symbol in string means:
-            ' ' - same item
-            '-' - removed item
-            '+' - added item
+        str: difference between files, where first symbols in string means:
+            '   ' - same item
+            '  -' - removed item
+            '  +' - added item
     """
     before = json.load(open(path_to_file1))
     after = json.load(open(path_to_file2))
@@ -24,14 +24,16 @@ def generate_diff(path_to_file1, path_to_file2):
     removed_keys = sorted(before.keys() - after.keys())
     added_keys = sorted(after.keys() - before.keys())
 
-    difference = []
+    difference = ['{']
 
     for key in same_keys:
         if before[key] == after[key]:
             difference.append(f'{SAME} {key}: {before[key]}')
         else:
-            difference.append(f'{ADDED} {key}: {after[key]}')
-            difference.append(f'{REMOVED} {key}: {before[key]}')
+            difference.extend([
+                f'{ADDED} {key}: {after[key]}',
+                f'{REMOVED} {key}: {before[key]}'
+            ])
 
     for key in removed_keys:
         difference.append(f'{REMOVED} {key}: {before[key]}')
@@ -39,4 +41,5 @@ def generate_diff(path_to_file1, path_to_file2):
     for key in added_keys:
         difference.append(f'{ADDED} {key}: {after[key]}')
 
+    difference.append('}')
     return '\n'.join(difference)
