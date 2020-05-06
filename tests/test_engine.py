e87220ddc8f2973f1c5c1c1c2e9561bfe7c701cd
Artlyne <Artlynee@gmail.com>
2020-05-06 13:49:44 +0500

    test: yaml files

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,10 +1,12 @@
-import pytest
-from gendiff import engine
+from gendiff.engine import generate_diff
 
-file1 = './tests/fixtures/test_before.json'
-file2 = './tests/fixtures/test_after.json'
-result = open('./tests/fixtures/test_expected_result.txt')
+json_file_before = './tests/fixtures/test_before.json'
+json_file_after = './tests/fixtures/test_after.json'
+yaml_file_before = './tests/fixtures/test_before.yaml'
+yaml_file_after = './tests/fixtures/test_after.yaml'
+result = open('./tests/fixtures/test_expected_result.txt').read()
 
 
-def test():
-    assert engine.generate_diff(file1, file2) == result.read()
+def test_flat_files():
+    assert generate_diff(json_file_before, json_file_after) == result
+    assert generate_diff(yaml_file_before, yaml_file_after) == result
