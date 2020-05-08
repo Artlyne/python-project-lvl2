10d73fc194e6e8fc0dbe1e3a1ef6f9f255697918
Artlyne <Artlynee@gmail.com>
2020-05-08 12:59:37 +0500

    rename: test files for flat tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,12 +1,13 @@
 from gendiff.engine import generate_diff
 
-json_file_before = './tests/fixtures/test_before.json'
-json_file_after = './tests/fixtures/test_after.json'
-yaml_file_before = './tests/fixtures/test_before.yaml'
-yaml_file_after = './tests/fixtures/test_after.yaml'
-result = open('./tests/fixtures/test_expected_result.txt').read()
+
+json_flat_file1 = './tests/fixtures/test_flat_before.json'
+json_flat_file2 = './tests/fixtures/test_flat_after.json'
+yaml_flat_file1 = './tests/fixtures/test_flat_before.yaml'
+yaml_flat_file2 = './tests/fixtures/test_flat_after.yaml'
+flat_result = open('./tests/fixtures/test_flat_result.txt').read()
 
 
 def test_flat_files():
-    assert generate_diff(json_file_before, json_file_after) == result
-    assert generate_diff(yaml_file_before, yaml_file_after) == result
+    assert generate_diff(json_flat_file1, json_flat_file2) == flat_result
+    assert generate_diff(yaml_flat_file1, yaml_flat_file2) == flat_result
