c49f686ed28bc4c10b536572b13a494197ee3565
Artlyne <Artlynee@gmail.com>
2020-06-07 23:44:09 +0500

    feat: new format - plain

---
+++ gendiff/form/plain.py
@@ -0,0 +1,29 @@
+def build_difference(difference, path=''):
+    diff = []
+
+    for key, (status, value) in sorted(difference.items()):
+        if status == 'nested':
+            path += f'{key}.'
+            diff.append(build_difference(value, path))
+
+        elif status == 'removed':
+            diff.append(f"Property '{path}{key}' was removed")
+
+        elif status == 'added':
+            if isinstance(value, dict):
+                value = 'complex value'
+            diff.append(f"Property '{path}{key}' was added with value: "
+                        f"'{value}'")
+
+        elif status == 'changed':
+            removed_value = value[0]
+            added_value = value[1]
+            diff.append(f"Property '{path}{key}' was changed. "
+                        f"From '{removed_value}' to '{added_value}'")
+
+    return '\n'.join(diff)
+
+
+def show_difference(difference):
+    diff = build_difference(difference)
+    return diff
