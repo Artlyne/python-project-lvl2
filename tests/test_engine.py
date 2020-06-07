1bcb7dfd9377e35e687937ea289da23da94c0402
Artlyne <Artlynee@gmail.com>
2020-06-07 23:46:08 +0500

    test: plain format

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,25 +1,41 @@
 from gendiff.engine import generate_diff
 
-
 json_flat_file1 = './tests/fixtures/test_flat_before.json'
 json_flat_file2 = './tests/fixtures/test_flat_after.json'
 yaml_flat_file1 = './tests/fixtures/test_flat_before.yaml'
 yaml_flat_file2 = './tests/fixtures/test_flat_after.yaml'
-flat_result = open('./tests/fixtures/test_flat_result.txt').read()
+extended_flat_result = open(
+    './tests/fixtures/test_extended_flat_result.txt').read()
+plain_flat_result = open('./tests/fixtures/test_plain_flat_result.txt').read()
 
 
 def test_flat_files():
-    assert generate_diff(json_flat_file1, json_flat_file2) == flat_result
-    assert generate_diff(yaml_flat_file1, yaml_flat_file2) == flat_result
+    assert generate_diff(json_flat_file1, json_flat_file2,
+                         'extended') == extended_flat_result
+    assert generate_diff(yaml_flat_file1, yaml_flat_file2,
+                         'extended') == extended_flat_result
+    assert generate_diff(json_flat_file1, json_flat_file2,
+                         'plain') == plain_flat_result
+    assert generate_diff(yaml_flat_file1, yaml_flat_file2,
+                         'plain') == plain_flat_result
 
 
 json_nested_file1 = './tests/fixtures/test_nested_before.json'
 json_nested_file2 = './tests/fixtures/test_nested_after.json'
 yaml_nested_file1 = './tests/fixtures/test_nested_before.yaml'
 yaml_nested_file2 = './tests/fixtures/test_nested_after.yaml'
-nested_result = open('./tests/fixtures/test_nested_result.txt').read()
+extended_nested_result = open(
+    './tests/fixtures/test_extended_nested_result.txt').read()
+plain_nested_result = open(
+    './tests/fixtures/test_plain_nested_result.txt').read()
 
 
 def test_nested_files():
-    assert generate_diff(json_nested_file1, json_nested_file2) == nested_result
-    assert generate_diff(yaml_nested_file1, yaml_nested_file2) == nested_result
+    assert generate_diff(json_nested_file1, json_nested_file2,
+                         'extended') == extended_nested_result
+    assert generate_diff(yaml_nested_file1, yaml_nested_file2,
+                         'extended') == extended_nested_result
+    assert generate_diff(json_nested_file1, json_nested_file2,
+                         'plain') == plain_nested_result
+    assert generate_diff(yaml_nested_file1, yaml_nested_file2,
+                         'plain') == plain_nested_result
