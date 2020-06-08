b2b1e9c7ae288fba1be9124f3938ff4f446010e2
Artlyne <Artlynee@gmail.com>
2020-06-08 20:32:40 +0500

    test: json format

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -7,6 +7,7 @@ yaml_flat_file2 = './tests/fixtures/test_flat_after.yaml'
 extended_flat_result = open(
     './tests/fixtures/test_extended_flat_result.txt').read()
 plain_flat_result = open('./tests/fixtures/test_plain_flat_result.txt').read()
+json_flat_result = open('./tests/fixtures/test_json_flat_result.txt').read()
 
 
 def test_flat_files():
@@ -18,6 +19,10 @@ def test_flat_files():
                          'plain') == plain_flat_result
     assert generate_diff(yaml_flat_file1, yaml_flat_file2,
                          'plain') == plain_flat_result
+    assert generate_diff(json_flat_file1, json_flat_file2,
+                         'json') == json_flat_result
+    assert generate_diff(yaml_flat_file1, yaml_flat_file2,
+                         'json') == json_flat_result
 
 
 json_nested_file1 = './tests/fixtures/test_nested_before.json'
@@ -28,6 +33,8 @@ extended_nested_result = open(
     './tests/fixtures/test_extended_nested_result.txt').read()
 plain_nested_result = open(
     './tests/fixtures/test_plain_nested_result.txt').read()
+json_nested_result = open(
+    './tests/fixtures/test_json_nested_result.txt').read()
 
 
 def test_nested_files():
@@ -39,3 +46,7 @@ def test_nested_files():
                          'plain') == plain_nested_result
     assert generate_diff(yaml_nested_file1, yaml_nested_file2,
                          'plain') == plain_nested_result
+    assert generate_diff(json_nested_file1, json_nested_file2,
+                         'json') == json_nested_result
+    assert generate_diff(yaml_nested_file1, yaml_nested_file2,
+                         'json') == json_nested_result
